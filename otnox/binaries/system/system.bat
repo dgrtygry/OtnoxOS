890233e17e422d7ec2668d78b77f5d7ba9d62235
Tick <76979689+dgrtygry@users.noreply.github.com>
2023-06-09 16:37:49 -0700

    Update system.bat

--- otnox/binaries/system/system.bat
+++ otnox/binaries/system/system.bat
@@ -1,1 +1,29 @@
+@echo off
+rem This is SYSTEM.BAT
 
+:menu
+cls
+echo 1. About
+echo 2. Exit
+echo.
+
+set /p choice=Enter your choice (1-2): 
+
+if "%choice%"=="1" (
+    echo Loading about information...
+    call about.asm
+    echo.
+    pause
+    goto menu
+)
+
+if "%choice%"=="2" (
+    echo Exiting...
+    call exit.bat
+)
+
+echo Invalid choice. Please try again.
+pause
+goto menu
+
+rem End of SYSTEM.BAT
